ea2992a153575731d0766b7687b872a4b5c6cf81
lswkim <lswkim322@gmail.com>
2021-12-14 00:07:26 +0900

    feat) change layouts

--- application/statics/js/pages/dashboard.js
+++ application/statics/js/pages/dashboard.js
@@ -1,28 +1,3 @@
-var optionsProfileVisit = {
-	annotations: {
-		position: 'back'
-	},
-	dataLabels: {
-		enabled:false
-	},
-	chart: {
-		type: 'bar',
-		height: 300
-	},
-	fill: {
-		opacity:1
-	},
-	plotOptions: {
-	},
-	series: [{
-		name: 'sales',
-		data: [9,20,30,20,10,20,30,20,10,20,30,20]
-	}],
-	colors: '#435ebe',
-	xaxis: {
-		categories: ["Jan","Feb","Mar","Apr","May","Jun","Jul", "Aug","Sep","Oct","Nov","Dec"],
-	},
-}
 let optionsVisitorsProfile  = {
 	series: [70, 30],
 	labels: ['긍정', '부정'],
@@ -45,73 +20,49 @@ let optionsVisitorsProfile  = {
 	}
 }
 
-var optionsEurope = {
-	series: [{
-		name: 'series1',
-		data: [310, 800, 600, 430, 540, 340, 605, 805,430, 540, 340, 605]
-	}],
+var areaOptions = {
+	series: [
+	  {
+		name: "긍정",
+		data: [31, 40, 28, 51, 42, 109, 100],
+	  },
+	  {
+		name: "부정",
+		data: [11, 32, 45, 32, 34, 52, 41],
+	  },
+	],
 	chart: {
-		height: 80,
-		type: 'area',
-		toolbar: {
-			show:false,
-		},
+	  height: 350,
+	  type: "area",
 	},
-	colors: ['#5350e9'],
-	stroke: {
-		width: 2,
-	},
-	grid: {
-		show:false,
-	},
+	colors: ['#55c6e8', '#FFCDD2'],
 	dataLabels: {
-		enabled: false
+	  enabled: false,
+	},
+	stroke: {
+	  curve: "smooth",
 	},
 	xaxis: {
-		type: 'datetime',
-		categories: ["2018-09-19T00:00:00.000Z", "2018-09-19T01:30:00.000Z", "2018-09-19T02:30:00.000Z", "2018-09-19T03:30:00.000Z", "2018-09-19T04:30:00.000Z", "2018-09-19T05:30:00.000Z", "2018-09-19T06:30:00.000Z","2018-09-19T07:30:00.000Z","2018-09-19T08:30:00.000Z","2018-09-19T09:30:00.000Z","2018-09-19T10:30:00.000Z","2018-09-19T11:30:00.000Z"],
-		axisBorder: {
-			show:false
-		},
-		axisTicks: {
-			show:false
-		},
-		labels: {
-			show:false,
-		}
-	},
-	show:false,
-	yaxis: {
-		labels: {
-			show:false,
-		},
+	  type: "datetime",
+	  categories: [
+		"2018-09-19T00:00:00.000Z",
+		"2018-09-19T01:30:00.000Z",
+		"2018-09-19T02:30:00.000Z",
+		"2018-09-19T03:30:00.000Z",
+		"2018-09-19T04:30:00.000Z",
+		"2018-09-19T05:30:00.000Z",
+		"2018-09-19T06:30:00.000Z",
+	  ],
 	},
 	tooltip: {
-		x: {
-			format: 'dd/MM/yy HH:mm'
-		},
+	  x: {
+		format: "dd/MM/yy HH:mm",
+	  },
 	},
-};
-
-let optionsAmerica = {
-	...optionsEurope,
-	colors: ['#008b75'],
-}
-let optionsIndonesia = {
-	...optionsEurope,
-	colors: ['#dc3545'],
-}
-
-
-
-var chartProfileVisit = new ApexCharts(document.querySelector("#chart-profile-visit"), optionsProfileVisit);
+  };
+ 
+var area = new ApexCharts(document.querySelector("#area"), areaOptions);
 var chartVisitorsProfile = new ApexCharts(document.getElementById('chart-visitors-profile'), optionsVisitorsProfile)
-var chartEurope = new ApexCharts(document.querySelector("#chart-europe"), optionsEurope);
-var chartAmerica = new ApexCharts(document.querySelector("#chart-america"), optionsAmerica);
-var chartIndonesia = new ApexCharts(document.querySelector("#chart-indonesia"), optionsIndonesia);
 
-chartIndonesia.render();
-chartAmerica.render();
-chartEurope.render();
-chartProfileVisit.render();
+area.render();
 chartVisitorsProfile.render()
